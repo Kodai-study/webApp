f0b9ed51fdcc9785cf4e2fce39a7d3fa420db25f
sugimasa <sugimasa@mail.com>
2023-10-12 16:09:57 +0900

    wakaran

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,6 +1,20 @@
-import type { FC } from 'react';
+import { NextResponse } from 'next/server';
+import * as mysql from 'promise-mysql';
 
-const FirstPost: FC = () => {
-	return <h1>First Post</h1>;
-};
-export default FirstPost;
+export async function GET() {
+  const connection = await mysql.createConnection({
+    host: '192.168.16.101',
+    port: 3306,
+    database: 'test',
+    user: 'AMS',
+    password: '2023r05T%'
+  });
+
+  const sql = 'SELECT * FROM m_employee';
+  const result = await connection.query(sql);
+  connection.end();
+
+  return NextResponse.json(result);
+}
+
+export default GET;
