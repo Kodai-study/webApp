556d5b722c2b151a60ed8528d7d65943e780be8d
Kodai-study <anchor.kou@softbank.ne.jp>
2023-10-16 15:02:44 +0900

    デバッグしてみる Linuxに送れるようPush

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,22 +1,12 @@
-import { NextResponse } from 'next/server';
 import * as mysql from 'promise-mysql';
 
-export async function GET() {
-  const connection = await mysql.createConnection({
-    host: '192.168.16.101',
-    port: 3306,
-    database: 'mysql',
-    user: 'AMS',
-    password: '2023r05T%'
-  });
+export default function Page() {
 
-  const sql = 'show tables;';
-  const result = await connection.query(sql);
-  connection.end();
+  console.log('test');
 
-  return JSON.stringify(result);
-}
-
-export default function Page({ users }) {
-  return GET();
+  return (
+    <>
+      <h1>Test</h1>
+    </>
+  )
 }
