b0228428f749fb23c1a7b85c7acbb2bcaf69ea3e
Kodai-study <anchor.kou@softbank.ne.jp>
2023-10-31 11:49:16 +0900

    Linuxのデータベースにアクセスできるように
    testページで、テストデータベースの、テーブルの一覧を表示

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,12 +1,22 @@
+import { NextResponse } from 'next/server';
 import * as mysql from 'promise-mysql';
 
-export default function Page() {
+export async function GET() {
+  const connection = await mysql.createConnection({
+    host: '192.168.16.101',
+    port: 3306,
+    database: 'test',
+    user: 'AMS',
+    password: '2023r05T%'
+  });
 
-  console.log('test');
+  const sql = 'show tables;';
+  const result = await connection.query(sql);
+  connection.end();
+      
+  return JSON.stringify(result);
+}
 
-  return (
-    <>
-      <h1>Test</h1>
-    </>
-  )
+export default function Page({ users }) {
+  return GET();
 }
---
+++ src/components/child.tsx
@@ -0,0 +1,7 @@
+export default function Child() {
+    return (
+        <div>
+            <h1>Child</h1>
+        </div>
+    );
+}
