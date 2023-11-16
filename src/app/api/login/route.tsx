5af25e46abc545e4e4dc1381f54cc1d8b0afcf5a
Kodai-study <anchor.kou@softbank.ne.jp>
2023-11-17 00:15:30 +0900

    クエリの型ガードを実装

--- src/app/api/login/route.tsx
+++ src/app/api/login/route.tsx
@@ -11,11 +11,56 @@ export async function GET(request: Request) {
   )
 }
 
+type LoginInformation = {
+  username: string;
+  password: string;
+}
+
+// リクエストボディがLoginInformation型に適合しているか検証する関数
+function isValidLoginInformation(data: any): data is LoginInformation {
+  return (
+    data &&
+    typeof data === 'object' &&
+    typeof data.username === 'string' &&
+    typeof data.password === 'string'
+  );
+}
+
 // ポストした時の処理
 export async function POST(request: Request) {
-  const body = await request.json()
-  return new Response(
-    await LogDB(body.id, body.password)
-  )
+
+  const body = await request.json();
+  if (!isValidLoginInformation(body)) {
+    return new Response(
+      JSON.stringify({ message: 'invalid request' }),
+      {
+        headers: {
+          'content-type': 'application/json;charset=UTF-8',
+        },
+        status: 400,
+      },
+    );
+  }
+  const result = await LogDB(body.username, body.password);
+  if (result) {
+    return new Response(
+      JSON.stringify({ message: 'login success' }),
+      {
+        headers: {
+          'content-type': 'application/json;charset=UTF-8',
+        },
+      },
+    );
+  } else {
+    return new Response(
+      JSON.stringify({ message: 'login failed' }),
+      {
+        headers: {
+          'content-type': 'application/json;charset=UTF-8',
+        },
+        status: 401,
+      },
+    );
+  }
 }
 
