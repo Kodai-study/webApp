7b3edc234be5d8855cfdf59b7618d6186661fcfe
Kodai-study <anchor.kou@softbank.ne.jp>
2023-11-17 00:15:12 +0900

    loginDBを、セキュアなクエリ実行に変更

--- src/app/api/login/loginDB.tsx
+++ src/app/api/login/loginDB.tsx
@@ -1,7 +1,7 @@
 import * as mysql from 'promise-mysql';
 
 //ログイン用データベース接続API
-export async function LogDB(id, pass) {
+export async function LogDB(id: string, pass: string) {
   const connection = await mysql.createConnection({
     host: '192.168.16.101',
     port: 3306,
@@ -10,19 +10,20 @@ export async function LogDB(id, pass) {
     password: 'test'
   });
   //社員番号とパスワード入力⇒持ってくるもの：名前　社員番号　パスワード　権限
-  const sql = 'select employee_name, t_login.employee_no, \
-               password, authority from m_employee inner join \
-               t_login on m_employee.employee_no = t_login.employee_no \
-               where m_employee.employee_no = ' + id + ';';
+  const sql = '\
+    SELECT employee_name, t_login.employee_no, password, authority\
+    FROM m_employee\
+    INNER JOIN t_login ON m_employee.employee_no = t_login.employee_no\
+    WHERE m_employee.employee_no = ?;';
   //const sql = 'select * from m_employee where employee_no = ' + 2 + ';';
-  const result = await connection.query(sql);
+  const result = await connection.query(sql, [id]);
   connection.end();
-  
+
   //パスワード比較する
-  if(pass != result[0].password){
+  if (pass != result[0].password) {
     return 0;
   }
-  else{
+  else {
     return 1;
   }
   // console.log(result[0].password);
