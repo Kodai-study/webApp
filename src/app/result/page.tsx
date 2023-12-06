e611f42b77490bce48cdd1b56ae399b7850fb14f
Koda-study <anchor.kou@softbank.ne.jp>
2023-12-06 16:35:22 +0900

    シリアル番号を受け取るクエリパラメータに、型ガードを追加

--- src/app/result/page.tsx
+++ src/app/result/page.tsx
@@ -1,8 +1,10 @@
 import DBGet1Colum from "./DB"
 import Image from 'next/legacy/image';
 import Link from 'next/link';
+import fs from 'fs';
 
 const Imgsrc = () => {
+
     return (
         <div>
             <Image
@@ -16,18 +18,41 @@ const Imgsrc = () => {
     );
 };
 
+const Test_GetFileSystemImage = () => {
+    let imageBuffer = fs.readFileSync('/mnt/win_share/imgs/yasi.jpg')
+    const base64Image = imageBuffer.toString('base64');
+
+    // HTMLで表示
+    const imageSrc = `data:image/jpeg;base64,${base64Image}`;
+    return <img src={imageSrc} alt="Image from Buffer" />;
+}
+
+type SearchParams = {
+    no: number;
+};
+
+function isSearchParams(params: any): params is SearchParams {
+    // params.noが数値に変換できるかどうか
+    return params.no !== undefined && !isNaN(Number(params.no));
+}
+
 const ResultPage = async ({ params, searchParams,
 }: {
     params: { slug: string };
-    searchParams: { [key: string]: string | string[] | undefined };
+    searchParams: SearchParams;
 }) => {
     try {
+        if (!isSearchParams(searchParams)) {
+            throw new Error('Invalid search params');
+        }
         const re = await DBGet1Colum(searchParams.no)
+
         const keys = Object.keys(re[0]);
         const propertiesString = keys.map(key => `${key}: ${re[0][key]}`).join(', ');
         return (
             <>
                 {propertiesString}
+                <Test_GetFileSystemImage />
                 <br></br>
                 <Imgsrc />
                 <Link href="/tracker">戻る</Link>
@@ -35,10 +60,16 @@ const ResultPage = async ({ params, searchParams,
         )
     }
     catch (error) {
-        const messege = ("シリアルナンバーが見つかりません");
+        let errorMessage = "";
+        if (error instanceof Error) {
+            errorMessage = `検索エラー : ${error.message}`;
+        }
+        else {
+            errorMessage = "未知のエラーです";
+        }
         return (
             <>
-                {messege}
+                {errorMessage}
                 <br></br>
                 <Link href="/tracker">戻る</Link>
             </>
