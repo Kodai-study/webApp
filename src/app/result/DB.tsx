3bfea3a5645b7145d846d93f651407b7feed0766
Koda-study <anchor.kou@softbank.ne.jp>
2023-12-06 15:55:19 +0900

    引数の名前とSQL文のリファクタリング

--- src/app/result/DB.tsx
+++ src/app/result/DB.tsx
@@ -1,7 +1,7 @@
 import * as mysql from 'promise-mysql';
 
 //ログイン用データベース接続API
-async function TrackDB(no) {
+async function TrackDB(workSerialNumber: number) {
     const connection = await mysql.createConnection({
         host: '192.168.16.101',
         port: 3306,
@@ -9,7 +9,7 @@ async function TrackDB(no) {
         user: 'test',
         password: 'test'
     });
-    const sql = `select * from m_work where serial_number = ${no};`;
+    const sql = `SELECT * FROM m_work WHERE serial_number = ${workSerialNumber};`;
 
     const result = await connection.query(sql);
     return result
