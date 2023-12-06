23512e309ddb82b8eb006f94ac80fbfffb41a02f
Koda-study <anchor.kou@softbank.ne.jp>
2023-12-06 17:53:36 +0900

    棒グラフが表示されるように

--- src/app/timestatistics/bar.tsx
+++ src/app/timestatistics/bar.tsx
@@ -2,19 +2,23 @@
 "use client"
 import React from 'react';
 import { Bar } from 'react-chartjs-2';
+import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend } from 'chart.js';
 
-const BarChart = ({ data }) => {
+ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
+
+
+const BarChart = ({ data }: { data: number[] }) => {
   const chartData = {
     labels: data.map((_, index) => `Data ${index + 1}`),
     datasets: [
       {
         label: 'Data',
+        data: data,
         backgroundColor: 'rgba(75,192,192,0.2)',
         borderColor: 'rgba(75,192,192,1)',
         borderWidth: 1,
         hoverBackgroundColor: 'rgba(75,192,192,0.4)',
         hoverBorderColor: 'rgba(75,192,192,1)',
-        data: data,
       },
     ],
   };
