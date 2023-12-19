93053544aa6ebb5aa55523a6ce60d8a7b12902ad
Koda-study <anchor.kou@softbank.ne.jp>
2023-12-19 19:47:19 +0900

    データベースへの接続は、環境変数を使って行うように変更
    /mnt/win_share/パスワード/.env.local  をプロジェクトルートに持ってくる

--- src/app/result/DB.tsx
+++ src/app/result/DB.tsx
@@ -3,11 +3,11 @@ import * as mysql from 'promise-mysql';
 //ログイン用データベース接続API
 async function TrackDB(workSerialNumber: number) {
     const connection = await mysql.createConnection({
-        host: '192.168.16.101',
-        port: 3306,
-        database: 'test',
-        user: 'test',
-        password: 'test'
+        host: process.env.DB_HOST,
+        port: Number(process.env.DB_PORT),
+        database: process.env.DB_NAME,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
     });
     const sql = `SELECT * FROM m_work WHERE serial_number = ${workSerialNumber};`;
 
